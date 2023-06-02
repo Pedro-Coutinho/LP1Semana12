9fec73e9f3054d94df04a88d6bdf66e057200417
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-06-02 12:48:32 +0100

    Ex 7

---
+++ WriteStuff2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace WriteStuff2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cycle = true;
+            string text;
+            StreamWriter sw = new StreamWriter(args[0]);
+            do
+            {
+                text = Console.ReadLine();
+
+                if(text == "")
+                    cycle = false;
+                else
+                    sw.WriteLine(text);;
+                
+            }
+            while(cycle);
+
+            sw.Close();
+        }
+    }
+}
