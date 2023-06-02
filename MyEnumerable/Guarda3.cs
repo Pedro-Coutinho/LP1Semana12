04257719fb56b602b9a5cd086c6e24177c274c32
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-06-02 11:31:16 +0100

    Ex 4

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T[] vars;
+        public Guarda3()
+        {
+            vars = new T[3];
+        }
+        public T GetItem(int i)
+        {
+            if(CheckI(i))
+            {
+                return vars[i];
+            }
+            else
+                throw new IndexOutOfRangeException();
+        }
+        public void SetItem(int i , T item)
+        {
+            if(CheckI(i))
+            {
+                vars[i] = item;
+            }
+            else
+                throw new IndexOutOfRangeException();
+        }
+
+        private bool CheckI(int i){
+            if(i<0 || i>2)
+                return false;
+            else
+                return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()  
+        {  
+            foreach (T o in vars)  
+            {  
+                yield return o;  
+            }  
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        } 
+
+    }
+}
