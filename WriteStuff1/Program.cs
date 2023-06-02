7c5c6f1b3523db9b96bbd4db403b73219be655ef
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-06-02 12:42:18 +0100

    Ex 6

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WriteStuff1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cycle = true;
+            List<string> strings = new List<string>();
+            int index = 0;
+            do
+            {
+                strings.Add(Console.ReadLine());
+
+                if(strings[index] == "")
+                    cycle = false;
+                else
+                    index++;
+                
+            }
+            while(cycle);
+
+            File.WriteAllLines(args[0], strings);
+        }
+    }
+}
